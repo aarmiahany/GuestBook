e7a19fee5157ae53ef4add1a0d1204a950617d0f
aarmiahany <41748878+aarmiahany@users.noreply.github.com>
2020-04-05 14:11:39 +0200

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,6 @@
+/* this file where all urls and app keys or secrets exists */
+/* feel free to change the port or provide url mongodb url */
+
 module.exports = {
     PORT: 5000,
     MongoURL: "mongodb+srv://armiahany:armia55@cluster0-u7dkf.mongodb.net/GuestBook?retryWrites=true&w=majority"
